6c1646c24a72a63789ec7f645eb7c1228fc2a123
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-04-05 12:35:50 +0200

    dodano funkcje getAllBlocks

---
+++ my-app/src/utils/getAllBlocks.ts
@@ -0,0 +1,16 @@
+interface Block {
+    x: number,
+    y: number,
+}
+
+function getAllBlocks(nrows: number, ncols: number): Block[] {
+    let result: Block[] = [];
+    for (let r = 0; r < nrows; r++) {
+        for (let c = 0; c < ncols; c++) {
+            result.push({ x: r, y: c });
+        }
+    }
+    return result;
+}
+
+export default getAllBlocks;
