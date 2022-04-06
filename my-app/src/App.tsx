b6742fe98efc5200f5ef46ad42d35e52bf354ff3
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-04-06 23:29:17 +0200

    dod. ruch weza + zmiana kier. ruchu po nacisn strzalek (e.g. <-, ->)

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,21 +1,48 @@
-import React, { useState } from 'react';
-import Canvas from './canvas/Canvas';
-import Block from './interfaces/Block';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Canvas from "./canvas/Canvas";
+import Block from "./interfaces/Block";
+import "./App.css";
+import shiftSnake from "./utils/shiftSnake";
+import Direction from "./types/Direction";
 
 function App() {
-
     // x - no of row, y - no of col of canvas (see Canvas element)
-    const [snake, setSnake]: [Block[], Function] = useState(
-        [
-            { x: 0, y: 2 },
-            { x: 0, y: 3 },
-            { x: 0, y: 4 },
-            { x: 1, y: 2 },
-            { x: 1, y: 3 },
-            { x: 1, y: 4 },
-        ]);
+    // x, y works similar to nrow, ncol in Python's pd.DataFrame
+    const [snake, setSnake]: [Block[], Function] = useState([
+        { x: 0, y: 4 },
+        { x: 0, y: 3 },
+        { x: 0, y: 2 },
+    ]);
     const [food, setFood]: [Block, Function] = useState({ x: 4, y: 4 });
+    const [moveDirection, setMoveDirection]: [Direction, Function] = useState(
+        Direction.Right
+    );
+
+    const keyDown = (event: KeyboardEvent): void => {
+        if (event.key === "ArrowLeft") {
+            setMoveDirection(Direction.Left);
+        }
+        if (event.key === "ArrowUp") {
+            setMoveDirection(Direction.Up);
+        }
+        if (event.key === "ArrowRight") {
+            setMoveDirection(Direction.Right);
+        }
+        if (event.key === "ArrowDown") {
+            setMoveDirection(Direction.Down);
+        }
+    };
+
+    document.addEventListener("keydown", keyDown);
+
+    useEffect(() => {
+        let timerId = setInterval(() => {
+            setSnake(shiftSnake(snake, moveDirection));
+        }, 1000);
+        return () => {
+            clearInterval(timerId);
+        };
+    });
 
     return (
         <div className="App">
