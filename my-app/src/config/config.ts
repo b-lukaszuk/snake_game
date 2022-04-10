60ed607ffeedb94c3e9defaae6cdb4990a8ad031
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-04-10 19:48:59 +0200

    dodano config.ts file z konfiguracja startowa gry

---
+++ my-app/src/config/config.ts
@@ -0,0 +1,12 @@
+let config = {
+    "nOfRows": 10,
+    "nOfCols": 10,
+    "snake": [
+        { "x": 0, "y": 4 },
+        { "x": 0, "y": 3 },
+        { "x": 0, "y": 2 },
+    ],
+    "food": { "x": 4, "y": 4 }
+}
+
+export default config;
