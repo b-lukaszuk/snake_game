6917a6ef208066ef2a4927450ff2493f64bbdf3d
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-05-01 10:54:47 +0200

    zmiana/uproszczenie funkcje jedzacej

--- my-app/src/utils/eatFood.ts
+++ my-app/src/utils/eatFood.ts
@@ -1,11 +1,8 @@
 import Block from '../interfaces/Block';
 
 function eatFood(snake: Block[], food: Block): Block[] {
-    let newSnake: Block[] = new Array(snake.length + 1);
-    newSnake[0] = food;
-    for (let i = 0; i < snake.length; i++) {
-        newSnake[i + 1] = snake[i];
-    }
+    let newSnake: Block[] = snake.slice();
+    newSnake.unshift(food);
     return newSnake;
 }
 
