efa16030a3bd50e44998b794ad613c502ebe13ef
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-05-30 18:57:08 +0200

    kosmetyka, bo linter narzekal

--- my-app/src/canvas/draw/displayGameOver.ts
+++ my-app/src/canvas/draw/displayGameOver.ts
@@ -1,7 +1,7 @@
 function displayGameOver(ctx: CanvasRenderingContext2D,
     canv: HTMLCanvasElement): void {
     ctx.clearRect(0, 0, canv.width, canv.height);
-    ctx.font = (canv.height / 8) + "px " + "Arial";
+    ctx.font = `${(canv.height / 8).toFixed(0)}px Arial`;
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
     ctx.fillText("Game Over", canv.width / 2, canv.height / 2);
