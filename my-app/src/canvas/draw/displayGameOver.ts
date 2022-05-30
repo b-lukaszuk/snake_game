c085e430a88e08961b6f77fdca8ea14ab22c5c2e
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-05-30 18:45:37 +0200

    dodanie funkcji rysujacej isGameOver na canvasie

---
+++ my-app/src/canvas/draw/displayGameOver.ts
@@ -0,0 +1,10 @@
+function displayGameOver(ctx: CanvasRenderingContext2D,
+    canv: HTMLCanvasElement): void {
+    ctx.clearRect(0, 0, canv.width, canv.height);
+    ctx.font = (canv.height / 8) + "px " + "Arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText("Game Over", canv.width / 2, canv.height / 2);
+}
+
+export default displayGameOver;
