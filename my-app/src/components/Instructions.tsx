e96753ec2bff4da2aba257c2cd2fbf0bcaaea87f
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-01 21:24:20 +0200

    kosmetyka

--- my-app/src/components/Instructions.tsx
+++ my-app/src/components/Instructions.tsx
@@ -2,15 +2,16 @@ import { ReactElement } from "react";
 
 const Instructions: React.FC = (): ReactElement<HTMLElement> => {
     return (
-        <p>
-            <b>Instructions: </b>
-            <br />
-            Click the button to start the game.
-            <br />
-            Arrow keys on your keyboard (&#8592; &#8593; &#8594; &#8595;) change the direction of the snake.
-            <br />
-            Collect food. Avoid collision with the walls and the snake's body.
-        </p>
+        <div>
+            <p> <b>Instructions: </b> </p>
+            <span>
+                Click the button to start the game.
+                <br />
+                Arrow keys on your keyboard (&#8592; &#8593; &#8594; &#8595;) change the direction of the snake.
+                <br />
+                Collect food. Avoid collision with the walls and the snake's body.
+            </span>
+        </div>
     )
 }
 
