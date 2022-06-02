e290ae0f74a74502aac52fb1a47e6251f9782267
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-02 21:28:35 +0200

    kosmetyka, doprecyzowanie instrukcji

--- my-app/src/components/Instructions.tsx
+++ my-app/src/components/Instructions.tsx
@@ -5,11 +5,14 @@ const Instructions: React.FC = (): ReactElement<HTMLElement> => {
         <div>
             <p> <b>Instructions: </b> </p>
             <span>
-                Click the button to start the game.
+                Click the button to start the game. The snake moves itself.
                 <br />
-                Arrow keys on your keyboard (&#8592; &#8593; &#8594; &#8595;) change the direction of the snake.
+                Arrow keys on your keyboard (&#8592; &#8593; &#8594; &#8595;) change the direction of the snake
+                <br />
+                (90 deg. right or left from the direction of the snake's head).
                 <br />
                 Collect food. Avoid collision with the walls and the snake's body.
+                <br />
             </span>
         </div>
     )
