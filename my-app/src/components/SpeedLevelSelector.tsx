f7407ecfcbccfbb72dd1c18de6de269c9b03b891
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-02 20:47:56 +0200

    dodano/poprawiono wyglad i dzialanie SpeedLevelSelector

--- my-app/src/components/SpeedLevelSelector.tsx
+++ my-app/src/components/SpeedLevelSelector.tsx
@@ -1,19 +1,69 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+
+import IRadioChoice from "../interfaces/IRadioChoice";
 
 import "./SpeedLevelSelector.css";
 
-const SpeedLevelSelector: React.FC = (): ReactElement<HTMLElement> => {
+interface Props {
+    choices: IRadioChoice[];
+    actionOnSelect: (chosenDelay: number) => void;
+}
+
+const SpeedLevelSelector: React.FC<Props> = (
+    props
+): ReactElement<HTMLElement> => {
+    const choices: IRadioChoice[] = props.choices;
+    const actionOnSelect: (chosenDelay: number) => void = props.actionOnSelect;
+
+    const [markedRadioVal, setMarkedRadioVal]: [string, Function] = useState(
+        choices[1].name
+    );
+
+    const getRadio = (choice: IRadioChoice): ReactElement<HTMLElement> => {
+        console.log("rendering new radio element", choice.name);
+        console.log("markedRadioVal is", markedRadioVal);
+        console.log(
+            "markedRadioVal === rendered element",
+            markedRadioVal === choice.name
+        );
+        return (
+            <span key={choice.id}>
+                <input
+                    type="radio"
+                    value={choice.name}
+                    name="speed"
+                    onChange={onSelect}
+                    checked={markedRadioVal === choice.name}
+                />
+                <label
+                    htmlFor={choice.name}
+                    onClick={() => {
+                        setMarkedRadioVal(choice.name);
+                        actionOnSelect(choice.delay);
+                    }}
+                >
+                    {choice.name}
+                </label>
+            </span>
+        );
+    };
+
+    const onSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setMarkedRadioVal(e.target.value);
+        choices.forEach((c) => {
+            if (c.name === markedRadioVal) {
+                actionOnSelect(c.delay);
+            }
+        });
+    };
+
     return (
         <fieldset>
             <legend>Choose speed level</legend>
-            <input type="radio" value="kindergarten" name="speed" />
-            <label htmlFor="kindergarten">Kindergarten</label>
-            <input type="radio" value="school" name="speed" />
-            <label htmlFor="school">School</label>
-            <input type="radio" value="college" name="speed" checked={true} />
-            <label htmlFor="college">College</label>
+            {choices.map((c) => getRadio(c))}
         </fieldset>
     );
-}
+};
 
 export default SpeedLevelSelector;
