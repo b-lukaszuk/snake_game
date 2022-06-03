a8c33bc3e8949145b8d4620fc637250bb43c22c1
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-03 22:52:44 +0200

    kosmetyka

--- my-app/src/components/Instructions.tsx
+++ my-app/src/components/Instructions.tsx
@@ -2,14 +2,12 @@ import { ReactElement } from "react";
 
 const Instructions: React.FC = (): ReactElement<HTMLElement> => {
     return (
-        <div>
+        <div >
             <p> <b>Instructions: </b> </p>
             <span>
-                After starting the game the snake moves itself.
+                Start the game and the snake will move itself.
                 <br />
-                Arrow keys (&#8592; &#8593; &#8594; &#8595;) change the direction of the snake
-                <br />
-                (90 deg. right or left from the direction of the snake's head).
+                Arrow keys (&#8592; &#8593; &#8594; &#8595;) change the direction of the snake (turn 90 deg. right or left).
                 <br />
                 Collect food. Avoid collision with the walls and the snake's body.
             </span>
